62f556d4ccc2512b7e4b708dc5f5dbbdb3189a61
Kornpob Bhirombhakdi <kbhirombhakdi@altalune.local>
2021-08-17 10:18:22 -0400

    v1.1.1

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="rolling_snapshot_proposal_editor",
+    version="1.1.1",
+    author="Kornpob Bhirombhakdi",
+    author_email="kbhirombhakdi@stsci.edu",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bkornpob/Rolling-Snapshot-Proposal-Editor.git",
+    package_data={'': ['./template/*','./demo/*']},
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+        ,"License :: OSI Approved :: MIT License"
+        ,"Operating System :: OS Independent"
+    ],
+    python_requires='>=3.'
+)
